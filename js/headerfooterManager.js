3ae1cfe582d114343202917e56f9922c6ae9d024
Pamela Amancio Goulart <pamela.ofc.ag@gmail.com>
2025-04-30 06:54:07 -0300

    reorganizacao de paginas

--- js/headerfooterManager.js
+++ js/headerfooterManager.js
@@ -3,7 +3,7 @@ const header = document.querySelector('header');
 const footer = document.querySelector('footer');
 
 // Carrega o arquivo header.html
-fetch('header.html')
+fetch('../components/header.html')
   .then(response => response.text())
   .then(data => {
     header.innerHTML = data;
@@ -11,9 +11,13 @@ fetch('header.html')
   .catch(error => console.error('Erro ao carregar o header:', error));
 
 // Carrega o arquivo footer.html
-fetch('footer.html')
+fetch('../components/footer.html')
   .then(response => response.text())
   .then(data => {
     footer.innerHTML = data;
   })
   .catch(error => console.error('Erro ao carregar o footer:', error));
+
+
+  
+// descobri que o carregamento do footer está bugando os 'nav-links'do header.  veja isso!!!
